225bb1623e3cada990118a3050fb966b163bc11a
alexrazo13 <alexrazo321@gmail.com>
2022-06-16 20:57:04 -0700

    Commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import { MoviesProvider } from "./features/Providers/MoviesProviders";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import FavoritesPage from './features/FavoriteMovies/pages/FavoritesPage';
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <MoviesProvider>
+        {/* <navbar /> add nav bar here */}
         <Switch>
           <Route exact path="/" component={LandingPage} />
         </Switch>
