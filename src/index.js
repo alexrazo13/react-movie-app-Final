c35fed929bc426e5cc4b62b7ad598666a02f5559
alexrazo13 <alexrazo321@gmail.com>
2022-06-16 20:54:49 -0700

    Commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import LandingPage from "./features/MovieSearch/pages/LandingPage";
+import { MoviesProvider } from "./features/Providers/MoviesProviders";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import FavoritesPage from './features/FavoriteMovies/pages/FavoritesPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <LandingPage />
+    <BrowserRouter>
+      <MoviesProvider>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+    </MoviesProvider>  
+  </BrowserRouter>
   </React.StrictMode>
 );
